8da852054538a76eef3c4a31ae8cb70b1e9af9e2
pedrosmv <pedrosmv@gmail.com>
2017-09-12 22:03:13 -0300

    12/7 - Finished the weather module, it now calculates the coefficient correctly

--- Code/Prototypes/getweather.cpp
+++ Code/Prototypes/getweather.cpp
@@ -6,36 +6,115 @@
 
 using namespace std;
 
+float calculate_coefficient(int weather[], float rain[]){
+  int i = 0;
+  float coefficient = 0;
+  for (i=0; i<12; i++){
+    coefficient = coefficient + (weather[i] * (1 -(rain[i]/100)));
+  }
+
+  return coefficient;
+}
+
 int main(){
   string stream;
-  int i;
-  int nclearday = 0;
+  int i=0;
+  int j=0;
+  int nclear = 0;
+  int nrain = 0;
+  int ncloudy = 0;
+  int nsunny = 0;
+  int nsnow = 0;
+  int novercast = 0;
+  int weather_coefficient[12];
+  float rain_percentage[12];
+  float coefficient;
 
-  system("curl wttr.in/'Campinas' > weather.txt");
+  system("curl wttr.in/'London' > weather.txt");
   ifstream weather("weather.txt");
-
-  stream.assign( (istreambuf_iterator<char>(weather)),
-                (istreambuf_iterator<char>()));
-
-  size_t clearday = stream.find("Clear", 0);
-  size_t clearnight = stream.find('clear-night');
-  size_t rain = stream.find('night');
-  size_t snow = stream.find('snow');
-  size_t sleet = stream.find('sleet');
-  size_t wind = stream.find('wind');
-  size_t fog = stream.find('fog');
-  size_t cloudy = stream.find('cloudy');
-  size_t partlycloudyday = stream.find('partly-cloudy-day');
-  size_t partlycloudynight = stream.find('partly-cloudy-night');
-  size_t thunderstorm = stream.find('thunderstorm');
-
-  while(clearday != string::npos){
-    nclearday++;
-    clearday = stream.find("Clear", clearday+1);
-
+  rain_percentage[0] = 0.0;
+  /* This loop will parse the file where the weather report is saved and for every
+  condition it will assign a value. These values are:
+    Sunny = 3
+    Clear = 2
+    Cloudy = 1
+    Overcast = 1
+    Rain = 0
+    Snow = 0
+  These values are coefficients that will be used in the decision whether the
+  selected tile will be irrigated or not. Besides the coefficients, the rain prediction
+  will be used as well to calculate a final coefficient for the given day. */
+  while ( i < 12 )
+  {
+    weather >> stream;
+    size_t sunny = stream.find("Sunny", 0);
+    if(sunny != string::npos)
+    {
+      nsunny++;
+      weather_coefficient[j] = 3;
+      j++;
+    }
+    size_t clear = stream.find("Clear", 0);
+    if(clear != string::npos)
+    {
+      nclear++;
+      weather_coefficient[j] = 2;
+      j++;
+    }
+    size_t rain = stream.find("Rain", 0);
+    if(rain != string::npos)
+    {
+      nrain++;
+      weather_coefficient[j] = 0;
+      j++;
+    }
+    rain = stream.find("rain", 0);
+    if(rain != string::npos)
+    {
+      nrain++;
+      weather_coefficient[j] = 0;
+      j++;
+    }
+    size_t cloudy = stream.find("Cloudy", 0);
+    if(cloudy != string::npos)
+    {
+      ncloudy++;
+      weather_coefficient[j] = 1;
+      j++;
+    }
+    cloudy = stream.find("cloudy", 0);
+    if(cloudy != string::npos)
+    {
+      ncloudy++;
+      weather_coefficient[j] = 1;
+      j++;
+    }
+    size_t snow = stream.find("Snow", 0);
+    if(snow != string::npos)
+    {
+      nsnow++;
+      weather_coefficient[j] = 0;
+      j++;
+    }
+    size_t overcast = stream.find("Overcast", 0);
+    if(overcast != string::npos)
+    {
+      novercast++;
+      weather_coefficient[j] = 1;
+      j++;
+    }
+    size_t found = stream.find('%');
+    if(found != string::npos)
+    {
+      float aux;
+      aux = stof(stream);
+      rain_percentage[i+1] = aux;
+      i++;
+    }
   }
-  cout << nclearday;
+  coefficient = calculate_coefficient(weather_coefficient, rain_percentage);
 
-
+  cout << coefficient;
+  weather.close();
   return 0;
 }
